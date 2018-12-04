61f724e25b7406ccadef0a7f27e8515fa1adc0c2
Lior Solomon <liors@optibus.com>
2018-12-04 17:32:06 +0200

    submit addresses

--- src/components/DeliveryForm.js
+++ src/components/DeliveryForm.js
@@ -1,24 +1,55 @@
 import React, {Component} from 'react';
 import LocationSearchInput from './LocationSearchInput';
 import '../styles/App.css';
+import Axios from 'axios';
 
 class DeliveryForm extends Component {
     constructor(props) {
         super(props);
         this.state = { address: '' };
     }
-     
-      render() {
-          const { origSelected, destSelected } = this.props;
-        return (
-          <form>
-              <LocationSearchInput onSelect={origSelected} placeholder="Origin" 
-                style={{padding: "10px", width: "100%"}}/>
-              <LocationSearchInput onSelect={destSelected} placeholder="Destination" 
-                style={{padding: "10px", width: "100%"}}/>
-          </form>
-        );
-      }
-}
 
-export default DeliveryForm;
+    submit = (event) => {
+        event.preventDefault();
+        const { origSelected, destSelected } = this.state;
+console.log({ origSelected, destSelected })
+        Axios.post('/newDelivery', {
+            origSelected, 
+            destSelected
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+        return false;
+    }
+    
+    origSelected = (selection) => {
+        const { origSelected } = this.props;
+        this.setState({origSelected: selection});
+        origSelected(selection);
+    }
+
+    destSelected = (selection) => {
+        const { destSelected } = this.props;
+        this.setState({destSelected: selection});
+        destSelected(selection);
+    }
+
+    render() {
+        //const { origSelected, destSelected } = this.props;
+        return (
+            <form onSubmit={this.submit}>
+                <LocationSearchInput onSelect={this.origSelected} placeholder="Origin" 
+                    style={{padding: "10px", width: "100%"}}/>
+                <LocationSearchInput onSelect={this.destSelected} placeholder="Destination" 
+                    style={{padding: "10px", width: "100%"}}/>
+                <button type="submit"></button>
+            </form>
+            );
+        }
+    }
+    
+    export default DeliveryForm;
